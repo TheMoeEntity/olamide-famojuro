a83bab8559c28cdae38f984c5ebef9d1b07da152
Moses Nwigberi <mosesnwigberi@gmail.com>
2025-09-16 16:00:23 +0100

    updated video link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import About from "@/components/About";
 
 export default function Home() {
   const youtubeUrl = `https://youtu.be/m2HrN-VhIHY`;
+  const firstSectionVideoLink = `https://youtu.be/pNLAADkyNvg?si=p5wUMvsF0zaLWwqE`;
   const filmUrl =
     "https://cdn.sanity.io/files/m0k79prg/production/2303d63c2e089d2ae33d050473b1d31acd3fd323.mp4";
   const mainImageUrl = `https://cdn.sanity.io/images/m0k79prg/production/9074149689037e294d64038605f09b5397bc3e89-1920x1920.jpg`;
@@ -17,10 +18,10 @@ export default function Home() {
         <VideoSection
           title="Films"
           description="Cinematic storytelling through 3D character animation and visual effects"
-          videoUrl="https://youtu.be/m2HrN-VhIHY" // Replace with your actual video URL
+          videoUrl={firstSectionVideoLink} // Replace with your actual video URL
           buttonText="View Portfolio"
           buttonHref="/films"
-          height="h-[550px]"
+          height="h-[650px]"
           showContent={false}
           showOverlay={false}
         />
